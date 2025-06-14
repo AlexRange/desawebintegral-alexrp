4cc543de09c9a1b4caf6d69ae8d22ee15aadca64
AlexRange <alejandrollamas2000@example.com>
2025-06-13 19:43:35 -0600

    TestNuevo

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -4,7 +4,7 @@ function App() {
        <>
        <NavBar />
         <div>
-           <h1>Bienvenido a mi página Web Hola Mundo Desarrollo Web Integral V2</h1>
+           <h1>Bienvenido a mi página Web Hola Mundo Desarrollo Web Integral V3</h1>
            <hr />
        </div>
        <main className='container'>
